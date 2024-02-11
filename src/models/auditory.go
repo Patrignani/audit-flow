fe10d3f760cb0ad728bff2ca9bfb92e70ae33171
DESKTOP-CQE42T6\Anderson <anderson.patrignani@gmail.com>
2024-02-11 20:44:47 -0300

    add IP

--- src/models/auditory.go
+++ src/models/auditory.go
@@ -3,12 +3,13 @@ package models
 import "time"
 
 type Auditory struct {
-	Id       string    `json:"id,omitempty" bson:"_id,omitempty"`
-	EntityId string    `json:"entityId" bson:"entity_id"`
-	Type     string    `json:"type" bson:"type"`
-	Entity   string    `json:"entity" bson:"entity"`
-	DatedIn  time.Time `json:"datedIn" bson:"dated_in"`
-	ClientId string    `json:"clientId" bson:"client_id"`
-	UserId   string    `json:"userId" bson:"user_id"`
-	Data     any       `json:"data" bson:"data"`
+	Id                   string    `json:"id,omitempty" bson:"_id,omitempty"`
+	EntityId             string    `json:"entityId" bson:"entity_id"`
+	Type                 string    `json:"type" bson:"type"`
+	Entity               string    `json:"entity" bson:"entity"`
+	DatedIn              time.Time `json:"datedIn" bson:"dated_in"`
+	ClientId             string    `json:"clientId" bson:"client_id"`
+	UserId               string    `json:"userId" bson:"user_id"`
+	InternetProtocolAddr string    `json:"internetProtocolAddr" bson:"internet_protocol_addr"`
+	Data                 any       `json:"data" bson:"data"`
 }
